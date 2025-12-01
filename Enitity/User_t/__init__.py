8338be6fab772712069def1c9667e6f9e621cad3
Suder Park <sgpark901@gmail.com>
2025-12-01 13:51:07 +0900

    typo

--- Enitity/User_t/__init__.py
+++ Enitity/User_t/__init__.py
@@ -1,4 +1,4 @@
-from .user_t import User
+from .user_t import User_t
 from .dto import UserCreateDto, UserLoginDto, UserUpdateDto
 
-__all__ = ["User", "UserCreateDto", "UserLoginDto", "UserUpdateDto"]
+__all__ = ["User_t", "UserCreateDto", "UserLoginDto", "UserUpdateDto"]
